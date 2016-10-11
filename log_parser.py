59a5c6cdba0a05495f5bdd4ddf33fa67f14529b8
cliebner <christina.liebner@gmail.com>
2016-10-10 18:28:36 -0700

    adds abstract class to read a time log file adds tcx-specific line cleaning method to account for wrapped lines and empty lines

--- log_parser.py
+++ log_parser.py
@@ -6,6 +6,15 @@ Started: 5 Oct 2016
 
 Define the most abstract log file with time-dependent data.
 
+Applications:
+For Tcx, we can parse the log file to gather data on how users are using TCx and what they are experiencing.
+Interesting stats:
+* number of NCUs/day
+* time to discover
+* time to do any other task
+* order of operations
+* number of crashes
+
 Assume:
 * Any file has a set of message types, where the number of message types is less than or equal to the number of messages (lines) in the file.
 * The user must define message types.  Any line that does not meet a message type definition is ignored.
@@ -20,6 +29,8 @@ Main loop:
 
 possible message types:
 * tcx action that has a start and an end
+** Discovery,
+** PushParamsHopefullyFaster: PushParamsHopefullyFaster: begin(), PushParamsHopefullyFaster: Run()
 * message pertaining to a specific (user defined by user input?) spc
 * broadcast message
 * sent messages
@@ -30,10 +41,11 @@ possible message types:
 * AT commands
 
 (custom datetimeFixer) Enforce dates:
-1. scan file, look for 00000 and grab dates
-2. compare timestamp right before first 0000 (t-1)
-3. if t-1 is > t at 0000, then assume it's from the day before, and assign to date of first 00000 minus 1 day
-4. hold onto date stamp from 0000 until next 00000 (or end of file) shows up, then replace date stamp with next one (or stop reading file)
+1. scan file, look for 00000 and grab dates. make look up table for line number and date
+2. between line numbers, apply date
+3. before first 0000, compare timestamp. if sequential, there's a good chance it's the same day. otherwise, assign previous day
+4. from last 00000 and end of file, apply last date
+5. print/save range of datetimes covered by file
 
 every line has:
 1. line number in file
@@ -48,19 +60,73 @@ d. send/receive message
 """
 import datetime as dt
 import matplotlib.pyplot as plt
+import pandas as pd
 
-class abstractTimeLogReader():
-    pass
 
-def abstractDateTimeFixer():
+class abstractTimeLogReader(object):
+    def __init__(self, filename):
+        self.TIMELOG_FILENAME = filename
+        self.timelog_lines = []
+
+        with open(filename, 'r', 0) as f:
+            self.timelog_lines = f.read().splitlines()
+        # f = open(self.TIMELOG_FILENAME, 'r', 0)
+        #
+        # # remove blank lines
+        # for line in f:
+        #     if len(line.strip()) > 0:
+        #         self.timelog_lines.append(line.strip())
+        # f.close()
+
+
+class TCX_TimeLogReader(abstractTimeLogReader):
+    # TCX_specific methods, or TCX-specific tweaks to methods in abstract
+    def __init__(self, filename):
+        super(TCX_TimeLogReader, self).__init__(filename)
+        self.TIME_FORMAT = '%H:%M:%S'
+        self.DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+        self.clean_df = self.clean_TCX_lines()
+
+    def clean_TCX_lines(self):
+
+        # make placeholder lists, to be converted to dataframe later (there has to be a faster way!!)
+        i = 0
+        line_num = []
+        session_num = []
+        time = []
+        msg = []
+        for line in self.timelog_lines:
+            if line.count('-') >= 2:
+                line_num.append(i)
+                session_num.append(line.partition('-')[0])
+                time.append(line.partition('-')[2].partition('-')[0])
+                msg.append(line.partition('-')[2].partition('-')[2])
+                i += 1
+            elif len(line.split('-')) == 1 and i > 0:  # line is not empty, not the first line, and wrapped from prev line
+                msg[i-1] += line
+            else:
+                pass
+
+        # make pandas dataframe with columns: line #, log session #, time, msg
+        return pd.DataFrame({'line_num': pd.Series(line_num),
+                             'session_num': pd.Series(session_num),
+                             'time': pd.Series(time),
+                             'msg': pd.Series(msg)})
+
+
+
+def abstractDateTimeFixer(object):
     # apply user-defined date time fixer method to the lines in the read-in file
     pass
 
-class abstractBucket():
+class abstractBucket(object):
     # do in a loop based on user input?
     # newBucketClass = bucketClassFactory()
     pass
 
-def bucketFactory():
+def bucketFactory(object):
     # make buckets based on user's rules! in a json perhaps??
     pass
+
+filename = 'TrackerCx_brian_2016-10-03.log'
+briantest = TCX_TimeLogReader(filename)
